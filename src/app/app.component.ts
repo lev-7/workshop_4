3288740027638d24c0a71f0aacc5568bcae3f8e7
Lev0071 <74816473+Lev0071@users.noreply.github.com>
2022-08-26 17:49:33 +1000

    Form submits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { User } from './model/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user:User[] = [
+
+    new User(1,"Ömer Demir","The lead male protagonist, and Elif's love interest and future husband.","https://pbs.twimg.com/profile_images/528535589616766976/anXl0SZ2_400x400.jpeg"),
+  
+    new User(2,"Elif Denizer","The lead female protagonist, and Ömer's love interest and future wife.","https://upload.wikimedia.org/wikipedia/commons/1/13/Elif_Denizer.jpg"),
+  
+    new User(3,"Nedret Denizer","The future female antagonist, Elif's aunt, and Ahmed's sister. ","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWG4E9TQpEbCXe1Sa6K5u6D8Q2Jiz2I3oCK8xseAAe7wmnVufjSA3vdwxDIJvcZAaCzqc&usqp=CAU")
+  
+  ]
+
   title = 'week4tut';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AccountComponent } from './account/account.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
